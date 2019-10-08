e0827800da7e9fb3f35fe144076f6fce1c8f3a0e
psidolphin <50124824+psidolphin@users.noreply.github.com>
2019-10-07 22:47:14 -0500

    Add files via upload

---
+++ src/jtc/Account.java
@@ -0,0 +1,41 @@
+/*
+ * A  Java Twitter Clone
+ * By  Jared Ren and Tyler Steward  * 
+ */
+package jtc;
+
+/**
+ *
+ * @author Jared Ren
+ */
+public class Account {
+    private String username;
+    private String password;
+
+    public Account(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ src/jtc/PrivateMessage.java
@@ -0,0 +1,13 @@
+/*
+ * A  Java Twitter Clone
+ * By  Jared Ren and Tyler Steward  * 
+ */
+package jtc;
+
+/**
+ *
+ * @author Jared Ren
+ */
+public class PrivateMessage extends Tweet {
+    
+}
